0286cb861b821085e5a4542d05c0a4f685c0a2f0
Deepika Srivastava <dpka9509@gmain.com>
2026-07-21 10:02:55 +0530

    [Deepika] Added: TopKLargest.java

---
+++ Heaps/TopKLargest.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class TopKLargest {
+
+    public static void main(String[] args) {
+
+        int[] transactions = {100, 50, 200, 80, 300, 250};
+        int k = 3;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int value : transactions) {
+
+            pq.add(value);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        while (!pq.isEmpty()) {
+            result.add(pq.poll());
+        }
+
+        Collections.reverse(result);
+
+        System.out.println(result);
+    }
+}

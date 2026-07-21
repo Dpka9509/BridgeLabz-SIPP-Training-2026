bf4c24fa80a58d07afb1a91b4c876426902c3b1a
Deepika Srivastava <dpka9509@gmain.com>
2026-07-21 10:04:17 +0530

    [Deepika] Added: KthLargestScore.java

---
+++ Heaps/KthLargestScore.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class KthLargestScore {
+
+    public static void main(String[] args) {
+
+        int[] scores = {95, 80, 70, 60, 50};
+        int k = 3;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int score : scores) {
+
+            pq.add(score);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        System.out.println("Kth largest score: " + pq.peek());
+    }
+}
